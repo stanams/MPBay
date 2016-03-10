3df1e00419b2c2529c21d6aa80cf3f228d511618
Your Actual Name <stan.amsellem@gmail.com>
2016-03-10 11:29:59 +0100

    refactor some jquery

--- javascript.js
+++ javascript.js
@@ -22,97 +22,28 @@ $(document).ready(function() {
           $("#pad" + (index + 1).toString()).addClass("note-key-pressed");
           $("#pad" + (index + 1).toString() + " .lightening-dot").addClass("lightening-dot-pressed");
         }
-      return;
-      switch (event.keyCode) {
-        // Q
-        case 81:
-          createjs.Sound.play(0);
-          $("#pad1").addClass("note-key-pressed");
-          $("#pad1 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 87:
-          createjs.Sound.play(1);
-          $("#pad2").addClass("note-key-pressed");
-          $("#pad2 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 69:
-          createjs.Sound.play(2);
-          $("#pad3").addClass("note-key-pressed");
-          $("#pad3 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 65:
-          createjs.Sound.play(3);
-          $("#pad4").addClass("note-key-pressed");
-          $("#pad4 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 83:
-          createjs.Sound.play(4);
-          $("#pad5").addClass("note-key-pressed");
-          $("#pad5 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 68:
-          createjs.Sound.play(5);
-          $("#pad6").addClass("note-key-pressed");
-          $("#pad6 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 90:
-          createjs.Sound.play(6);
-          $("#pad7").addClass("note-key-pressed");
-          $("#pad7 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 88:
-          createjs.Sound.play(7);
-          $("#pad8").addClass("note-key-pressed");
-          $("#pad8 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-        case 67:
-          createjs.Sound.play(8);
-          $("#pad9").addClass("note-key-pressed");
-          $("#pad9 .lightening-dot").addClass("lightening-dot-pressed");
-          break;
-      }
+
     });
 
     $(document).bind("keyup", function(event){
-      switch (event.keyCode) {
-        // Q
-        case 81:
-          $("#pad1").removeClass("note-key-pressed");
-          $("#pad1 .lightening-dot").removeClass("lightening-dot-pressed");
-          // $(".container-single-pad").removeClass(".container-single-pad-pressed");
-          break;
-        case 87:
-          $("#pad2").removeClass("note-key-pressed");
-          $("#pad2 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 69:
-          $("#pad3").removeClass("note-key-pressed");
-          $("#pad3 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 65:
-          $("#pad4").removeClass("note-key-pressed");
-          $("#pad4 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 83:
-          $("#pad5").removeClass("note-key-pressed");
-          $("#pad5 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 68:
-          $("#pad6").removeClass("note-key-pressed");
-          $("#pad6 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 90:
-          $("#pad7").removeClass("note-key-pressed");
-          $("#pad7 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 88:
-          $("#pad8").removeClass("note-key-pressed");
-          $("#pad8 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
-        case 67:
-          $("#pad9").removeClass("note-key-pressed");
-          $("#pad9 .lightening-dot").removeClass("lightening-dot-pressed");
-          break;
+
+      var helper = {
+        81: 0,
+        87: 1,
+        69: 2,
+        65: 3,
+        83: 4,
+        68: 5,
+        90: 6,
+        88: 7,
+        67: 8,
+      }
+
+      var index = helper[event.keyCode];
+      if (index || (index === 0)) {
+
+        $("#pad" + (index + 1).toString()).removeClass("note-key-pressed");
+        $("#pad" + (index + 1).toString() + " .lightening-dot").removeClass("lightening-dot-pressed");
       }
 
       $('#track1').click(function(){
