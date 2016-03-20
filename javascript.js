fe5fcf726ecb302e923e6d6ff9c9f37c02a1eb6e
Stan <stan.amsellem@gmail.com>
2016-03-20 13:41:27 +0100

    minor updates

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     // Assign a sample to each pad
     $(document).bind("keydown", function(event){
+
       var helper = {
         81: 0,
         87: 1,
@@ -23,7 +24,6 @@ $(document).ready(function() {
           $("#pad" + (index + 1).toString()).addClass("note-key-pressed");
           $("#pad" + (index + 1).toString() + " .lightening-dot").addClass("lightening-dot-pressed");
         }
-
     });
 
 
@@ -44,58 +44,55 @@ $(document).ready(function() {
 
       var index = helper[event.keyCode];
       if (index || (index === 0)) {
-
         $("#pad" + (index + 1).toString()).removeClass("note-key-pressed");
         $("#pad" + (index + 1).toString() + " .lightening-dot").removeClass("lightening-dot-pressed");
       }
-
     });
 
     $(".app-title").click(function(){
       location.reload();
     });
 
+
     // The following code stands for the different steps
     // of the tutorial when clicking on the NEXT button
-
-    $("#next-step-zero").on("click", function(){
-      $("#step-one").css("z-index", 2);
-      $('#pad1').addClass("step-one-pad-background");
-      for (var i = 0; i < 10; i++) {
-        $('.pad' + i.toString() + '-class-key').hide();
-      }
+    $("#next-step-0").on("click", function(){
+     $("#step-1").css("z-index", 2);
+     $('#pad1').addClass("step-1-pad-background");
     });
 
-    $("#next-step-one").on("click", function(){
-      $("#step-two").css("z-index", 3);
-      $('#pad1').removeClass("step-one-pad-background");
-      $('#pad2').addClass("step-two-pad-background");
+    $("#next-step-1").on("click", function(){
+      $("#step-2").css("z-index", 3);
+      $('#pad1').removeClass("step-1-pad-background");
+      $('#pad2').addClass("step-2-pad-background");
     });
 
-    $("#next-step-two").on("click", function(){
-      $("#step-three").css("z-index", 4);
-      $('#pad3').addClass("step-three-pad-background");
-      $('#pad4').addClass("step-three-pad-background");
-      $('#pad2').removeClass("step-two-pad-background");
+    $("#next-step-2").on("click", function(){
+      $("#step-3").css("z-index", 4);
+      $('#pad3').addClass("step-3-pad-background");
+      $('#pad4').addClass("step-3-pad-background");
+      $('#pad2').removeClass("step-2-pad-background");
     });
 
-    $("#next-step-three").on("click", function(){
-      $("#step-four").css("z-index", 5);
-      $('#pad5').addClass("step-four-pad-background");
-      $('#pad6').addClass("step-four-pad-background");
-      $('#pad3').removeClass("step-three-pad-background");
-      $('#pad4').removeClass("step-three-pad-background");
+    $("#next-step-3").on("click", function(){
+      $("#step-4").css("z-index", 5);
+      $('#pad5').addClass("step-4-pad-background");
+      $('#pad6').addClass("step-4-pad-background");
+      $('#pad3').removeClass("step-3-pad-background");
+      $('#pad4').removeClass("step-3-pad-background");
     });
 
-    $("#next-step-four").on("click", function(){
-      $("#step-five").css("z-index", 6);
-      $('#pad5').removeClass("step-four-pad-background");
-      $('#pad6').removeClass("step-four-pad-background");
-      $('#pad7').addClass("step-five-pad-background");
-      $('#pad8').addClass("step-five-pad-background");
-      $('#pad9').addClass("step-five-pad-background");
+    $("#next-step-4").on("click", function(){
+      $("#step-5").css("z-index", 6);
+      $('#pad5').removeClass("step-4-pad-background");
+      $('#pad6').removeClass("step-4-pad-background");
+      $('#pad7').addClass("step-5-pad-background");
+      $('#pad8').addClass("step-5-pad-background");
+      $('#pad9').addClass("step-5-pad-background");
     });
 
+
+    // Keys helper icon
     $("#help-icon").on("click", function(){
       for (var i = 0; i < 10; i++) {
         $('.pad' + i.toString() + '-class-key').toggle();
